3a35fc3688e49d7d091ec1db262dc80fd938dc7b
Shohjahon Komiljonov <komiljonovshohjahon@gmail.com>
2021-11-23 16:03:08 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
-import '../public/main.dart'
 
 export default function Home() {
   return (
