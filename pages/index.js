11e135df38a134e2a0027ea7dbf5a6e752875ce5
Shohjahon Komiljonov <komiljonovshohjahon@gmail.com>
2021-11-23 16:01:25 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -23,33 +23,9 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="/yollet-web" className={styles.card}>
-            <h2>YolletWeb &rarr;</h2>
-            <p>Go to YolletWeb Admin Dashboard</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <Link href="/yollet-web" className={styles.card}>
+            <a>YolletWeb</a>
+          </Link>
         </div>
       </main>
 
